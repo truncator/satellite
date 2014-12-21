498ba05a7d21c8c4cd4a21966d3300c57a085d61
Truncator <truncator0@gmail.com>
2014-12-21 07:48:40 -0600

    Enable texture edge clamping.

--- Source/Graphics/Texture.cpp
+++ Source/Graphics/Texture.cpp
@@ -27,6 +27,8 @@ void Texture::Load(const std::string& name)
 
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image_data);
 
